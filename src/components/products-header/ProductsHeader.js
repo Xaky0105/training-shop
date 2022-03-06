9a97d4c283a8761e2e686416df4520aad155744b
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-03-06 20:38:15 +0300

    delete useEffect from ProductsHeader

--- src/components/products-header/ProductsHeader.js
+++ src/components/products-header/ProductsHeader.js
@@ -8,7 +8,7 @@ import {Link} from 'react-router-dom'
 
 import Filter from '../filter/Filter'
 
-import { useState, useEffect} from 'react'
+import { useState} from 'react'
 import classNames from 'classnames'
 
 
